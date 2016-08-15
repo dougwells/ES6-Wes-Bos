209ae169bf4cca28a1b11acd7d96cffe2943f88c
dougwells <dougwells@gmail.com>
2016-08-15 07:58:16 -0600

    Event Emitter - final commit

--- eventEmitter.js
+++ eventEmitter.js
@@ -1,5 +1,10 @@
 "use strict"
 
+//Steps
+  //build constructor with object for every eventName
+  // 'on' method: creates property that merely holds array of functions associated w/that eventName
+  // 'trigger' method:  iterates over array of FNs created with 'on' & invokes them.
+
 class EventEmitter {
   constructor (){
     this.events = {};
@@ -16,7 +21,8 @@ class EventEmitter {
   trigger (eventName, ...rest){
     if(this.events[eventName]){
       this.events[eventName].forEach(cb =>{
-        cb.apply(null, rest);
+        cb.apply(null, rest); //invokes cb with arguments from rest
+        // cb();
       })
     }
   }
